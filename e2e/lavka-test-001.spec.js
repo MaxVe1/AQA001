71cb4a76fb07d28b9fc08b845930d7fb4fd02045
100nout <emvi@tut.by>
2026-02-27 20:00:12 +0300

    add item to cart

--- e2e/lavka-test-001.spec.js
+++ e2e/lavka-test-001.spec.js
@@ -6,18 +6,15 @@ test('lavka-test', async ({ page }) => {
     
    await page.locator('body > div.v-modal.sale > div > span').click();
    await page.getByText('Каталог').click();
-
    await page.locator('a[href="/catalog/cat-odezda-1782510"]').click(); 
    
-   //await page.getByRole('link', { name: 'Одежда' }).click();
-   //await page.locator('a[href="/catalog/cat-platia-i-sarafany-38365"]').click(); 
-   //await page.getByText('Платья и сарафаны').click();
-   //await page.locator('//span[contains(text(),"Платья и сарафаны")]').click();
-   //*[contains(text(),'ABC')]
-   
+    
    await page.locator('#sidebar > div > div:nth-child(1) > div.filter__body > ul > li:nth-child(1)').click();
-
    await page.locator('.filter__text').getByText('Платья и сарафаны').click(); 
+
+   await page.locator(`xpath=//span[contains(text(),"5805")]/../../../div`).click();    
+   await page.locator('.options-selector__item').getByText("42").click(); 
+   await page.locator('.button').getByText('Выбрать').click();
      
  });
  
