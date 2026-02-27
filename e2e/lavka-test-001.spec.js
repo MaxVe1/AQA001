1d189fd3ccc31f7115855bbecd63ba0524a5fe8e
100nout <emvi@tut.by>
2026-02-27 19:34:35 +0300

    upd-1

--- e2e/lavka-test-001.spec.js
+++ e2e/lavka-test-001.spec.js
@@ -7,7 +7,17 @@ test('lavka-test', async ({ page }) => {
    await page.locator('body > div.v-modal.sale > div > span').click();
    await page.getByText('Каталог').click();
 
-   await page.locator('a[href="/catalog/cat-odezda-1782510"]').click();   
+   await page.locator('a[href="/catalog/cat-odezda-1782510"]').click(); 
+   
+   //await page.getByRole('link', { name: 'Одежда' }).click();
+   //await page.locator('a[href="/catalog/cat-platia-i-sarafany-38365"]').click(); 
+   //await page.getByText('Платья и сарафаны').click();
+   //await page.locator('//span[contains(text(),"Платья и сарафаны")]').click();
+   //*[contains(text(),'ABC')]
+   
+   await page.locator('#sidebar > div > div:nth-child(1) > div.filter__body > ul > li:nth-child(1)').click();
+
+   await page.locator('.filter__text').getByText('Платья и сарафаны').click(); 
      
  });
  
