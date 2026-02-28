9447cfbf52387ea4881e991526702161059aef16
100nout <emvi@tut.by>
2026-02-28 11:54:55 +0300

    add comments

--- e2e/lavka-test-001.spec.js
+++ e2e/lavka-test-001.spec.js
@@ -6,21 +6,23 @@ const mainURL = "https://ramonki.by/";
 
 test('lavka-test', async ({ page }) => {
    await page.goto(mainURL);
-    
+
+   //Remove pop-up close  
    await page.locator('body > div.v-modal.sale > div > span').click();
+   //Navigation
    await page.getByText('Каталог').click();
    await page.locator('a[href="/catalog/cat-odezda-1782510"]').click(); 
    
-    
+   //Navigation
    await page.locator('#sidebar > div > div:nth-child(1) > div.filter__body > ul > li:nth-child(1)').click();
    await page.locator('.filter__text').getByText('Платья и сарафаны').click(); 
 
-   
+   //Добавление товаров (выбо артикула, размеров)
    await page.locator(`xpath=//span[contains(text(),${artikulNumber})]/../../../div`).click();    
    await page.locator('.options-selector__item').getByText(size).click(); 
    await page.locator('.button').getByText('Выбрать').click();
 
-
+   //Проверки заказанных товаров (номер артикул, цена)
    await expect(page.locator('.success-add-modal__header')).toHaveText('Товар добавлен в корзину');
    await expect(page.locator('.success-add-modal__text--title')).toContainText(`${artikulNumber}`);  
    await expect(page.locator('//p[contains(text(),"артикул")]')).toContainText(`${artikulNumber}`);    
